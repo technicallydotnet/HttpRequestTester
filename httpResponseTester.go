9b7adcf6b91e8f37e06c43ba4d19b805bb2dbbfe
Boolean <jamesglennewton@gmail.com>
2021-12-14 12:25:30 +0000

    Add files via upload

--- httpResponseTester.go
+++ httpResponseTester.go
@@ -27,6 +27,8 @@ func main() {
 	flag.Parse()
 	println("Performing " + httpType + " request at " + URL + " for " + strconv.Itoa(numberOfTimes) + " times")
 
+	httpType = strings.ToLower(httpType)
+
 	var statusMessages [47]string = [47]string{"ok", "created", "accepted", "no content", "multiple Choice", "moved Permanently", "found", "seeOther", "not Modified", "temporary Redirect", "permenant Redirect", "bad Request", "unauthorized", "forbidden", "not Found", "method Not Allowed", "not Accepted", "proxy Auth Required", "conflict", "gone", "length Required", "precondition Failed", "payload Too Large", "URI Too Long", "Unsupported Media Type", "Range Not Satisfiable", "Im A Teapot", "misdirected Response", "unprocessed Entity", "locked", "failed Dependency", "upgrade Required", "precondition Required", "too Many Requests", "request Header Fields Too Large", "unavailable For Legal Reasons", "interal Server Error", "not Implemented", "bad Gateway", "service Unavailable", "gateway Timeout", "http Version Not Supported", "variant Also Negotiates", "insufficient Storage", "loop Detected", "not Extended", "network Auth Required"}
 	var statusMessagesCount [47]int = [47]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	var statusCodes [48]int = [48]int{200, 201, 202, 204, 300, 301, 302, 303, 304, 307, 308, 400, 401, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 418, 421, 422, 423, 424, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511}
@@ -79,7 +81,7 @@ func performHttpRequest(httpType string, numberOfTimes int, URL string, statusMe
 			time.Sleep(time.Duration(delay) * time.Millisecond)
 		}
 	} else {
-		println("http type not recognised")
+		println("http type not recognised or supported")
 	}
 	return statusMessagesCount
 }
